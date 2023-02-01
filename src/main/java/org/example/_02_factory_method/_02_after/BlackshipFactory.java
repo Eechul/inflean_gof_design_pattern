fde7e7b1e51f7b1b4a0accf135fc5f8020baf7a7
이동철 <choise154@gmail.com>
2023-02-01 20:56:05 +0900

    팩토리 메서드 부분 수정

--- src/main/java/org/example/_02_factory_method/_02_after/BlackshipFactory.java
+++ src/main/java/org/example/_02_factory_method/_02_after/BlackshipFactory.java
@@ -7,4 +7,9 @@ public class BlackshipFactory implements ShipFactory {
         // 알맞는 Product 클래스를 만든 후, 리턴해준다.
         return new Blackship();
     }
+
+    @Override
+    public void sendEmailTo(String email, Ship ship) {
+        System.out.println(ship.getName() + " 다 만들었습니다.");
+    }
 }
